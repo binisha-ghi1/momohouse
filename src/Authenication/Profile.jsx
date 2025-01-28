a52f7757c5857c301f3200ec8110cfe847e8e0e5
Binisha Ghimire <binishaghimire126@gmail.com>
2025-01-28 20:17:51 +0545

    first commit

--- src/Authenication/Profile.jsx
+++ src/Authenication/Profile.jsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
+
 function Profile() {
   const { user, isAuthenticated, isLoading } = useAuth0();
   console.log(user, user)
@@ -12,10 +13,11 @@ function Profile() {
     <div>
     {isAuthenticated ? (
       <div className="flex flex-col ml-10 mt-10 mb-10 h-64 w-32 border-slate-500">
-        <h2 className="text-2xl font-bold mt-4 ">Welcome, {user.name}</h2>
-        <p className="text-2xl font-bold mt-2">Email: {user.email}</p>
-        <img className="rounded-lg border-black"
+        <h2 className="text-2xl font-bold mt-4 ">Welcome, <br/>
+        <img className="rounded-full border-black"
          src={user.picture} alt="profile_picture" />
+         {user.name}</h2>
+        <p className="text-2xl font-bold mt-2">Email: {user.email}</p>
       </div>
     ) : (
       <div>
