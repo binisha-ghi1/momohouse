0fbc176bcc7ee36db5d181235f016b7f073c3047
Binisha Ghimire <binishaghimire126@gmail.com>
2025-01-29 08:59:57 +0545

    update

--- src/main.jsx
+++ src/main.jsx
@@ -9,16 +9,15 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
-      <Auth0Provider
-        domain="dev-8umwky1w7im1pu2x.us.auth0.com"
-        clientId="ZqeimAeAjs3dbOvTy41znX7aYcXowkP4"
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}
-      >
-      ,
-      <App />
-      </Auth0Provider>
+    <Auth0Provider
+    domain="dev-8umwky1w7im1pu2x.us.auth0.com"
+    clientId="ZqeimAeAjs3dbOvTy41znX7aYcXowkP4"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>,
     </CartProvider>
   </BrowserRouter>
 );
