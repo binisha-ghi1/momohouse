5fc7cad3aab49ae7667e34569b57bcdb178ba7fb
Binisha Ghimire <binishaghimire126@gmail.com>
2025-01-29 14:15:26 +0545

    update

--- src/Authenication/Profile.jsx
+++ src/Authenication/Profile.jsx
@@ -12,26 +12,20 @@ function Profile() {
   return (
     <div>
     {isAuthenticated ? (
-      <div className="flex flex-col bg-gray-200  ml-10 mt-10 mb-10 h-64 w-full ">
+      <div className="flex flex-col bg-gray-200   mt-10 mb-10 h-64 w-full ">
         <h2 className="text-2xl font-bold mt-4 ">Welcome, <br/>
         <img className="rounded-full border-white"
          src={user.picture} alt="profile_picture" /> </h2>
          
 
-         <div className="flex flex-col justify-center items-center mt-6 font-bold text-2xl">
-          <h2> {user.name}</h2>
+         <div className="flex flex-col  mt-6 font-bold text-2xl">
+          <h2> Name : {user.name}</h2>
          </div>
-        <div className="flex flex-col justify-center items-center mt-6 font-bold text-2xl"> 
+        <div className="flex flex-col  mt-6 font-bold text-2xl"> 
         <p className="text-2xl font-bold mt-2">Email: {user.email}</p>
         </div>
-        <button
-          onClick={() =>
-            loginWithRedirect()
-          }
-          type="button"
-          className="flex items-center gap-2 justify-center border-2 border-slate-500 mt-3 w-full py-2 px-4 text-black font-medium rounded-md hover:text-white hover:bg-green-700"
-        >
-          </button>
+        
+          
       </div>
     ) : (
       <div>
